ecc89445e13a391e62457f0cd9aae90d01cac92f
Dmitry Sagoyan <dmitry.sagoyan@eleven.de>
2024-07-14 19:08:09 +0200

    refactor: code improvement in ResourceMgr

--- src/core/manager/ResourceMgr.py
+++ src/core/manager/ResourceMgr.py
@@ -20,12 +20,11 @@ from src.utils.logger import logger
 
 
 class ResourceMgr:
-
     def __init__(
-            self,
-            settings: dict,
-            feature_extract_mgr: FeatureExtractMgr,
-            domainquery_mgr: DomainQueryMgr,
+        self,
+        settings: dict,
+        feature_extract_mgr: FeatureExtractMgr,
+        domainquery_mgr: DomainQueryMgr,
     ):
         self.settings = settings
         self.feature_extract_mgr = feature_extract_mgr
@@ -48,7 +47,7 @@ class ResourceMgr:
 
     @staticmethod
     def get_resources_by_status(
-            sess: Session, status: str
+        sess: Session, status: str
     ) -> Query[Resource] | Query[Any]:
         return sess.query(Resource).filter_by(status=status)
 
@@ -58,7 +57,7 @@ class ResourceMgr:
 
     @staticmethod
     def get_checked_resource_by_value(
-            sess: Session, value: str
+        sess: Session, value: str
     ) -> Type[Resource] | None:
         return (
             sess.query(Resource)
@@ -72,7 +71,7 @@ class ResourceMgr:
 
     @staticmethod
     def get_checked_propogated_resource_by_value(
-            sess: Session, domain_name: str
+        sess: Session, domain_name: str
     ) -> Type[Resource] | None:
         return (
             sess.query(Resource)
@@ -90,7 +89,7 @@ class ResourceMgr:
 
     @staticmethod
     def get_checked_with_truerate_resources(
-            sess: Session,
+        sess: Session,
     ) -> Query[Type[Resource]] | Query[Any]:
         return (
             sess.query(Resource)
@@ -100,7 +99,7 @@ class ResourceMgr:
 
     @staticmethod
     def get_checked_with_predictionrate_resources(
-            sess: Session,
+        sess: Session,
     ) -> Query[Type[Resource]] | Query[Any]:
         return (
             sess.query(Resource)
@@ -110,7 +109,7 @@ class ResourceMgr:
 
     @staticmethod
     def prepare_domain_name_for_search(
-            domain_name: str, domain_endings_langs: list, character: str
+        domain_name: str, domain_endings_langs: list, character: str
     ) -> str:
         # TODO check this function and make it more clear what it does
         domain_parts = domain_name.split(".")
@@ -141,7 +140,7 @@ class ResourceMgr:
 
     @debug_log_entry_exit(__name__)
     def _get_requested_checked_resource(
-            self, sess: Session, resource: Resource
+        self, sess: Session, resource: Resource
     ) -> Type[Resource] | None:
         domain_name = None
 
@@ -189,11 +188,11 @@ class ResourceMgr:
 
     @debug_log_entry_exit(__name__)
     def get_and_predict(
-            self,
-            sess: Session,
-            resource: Resource,
-            recheck: bool = False,
-            show_top_features: bool = False,
+        self,
+        sess: Session,
+        resource: Resource,
+        recheck: bool = False,
+        show_top_features: bool = False,
     ):
         # if requested resource is not found
         #   1. add resource as check, then start feature extraction
@@ -206,9 +205,9 @@ class ResourceMgr:
         # if the requested resource was not found or recheck is given or top features are requested, and we don't
         # have them
         has_top_features = (
-                requested_resource
-                and show_top_features
-                and not requested_resource.top_features
+            requested_resource
+            and show_top_features
+            and not requested_resource.top_features
         )
 
         if requested_resource is None or recheck or has_top_features:
@@ -252,12 +251,12 @@ class ResourceMgr:
 
     @debug_log_entry_exit(__name__)
     def process_requested_resource(
-            self,
-            sess: Session,
-            res: Resource,
-            analyse_resource=True,
-            predict_resource=True,
-            show_top_features=False,
+        self,
+        sess: Session,
+        res: Resource,
+        analyse_resource=True,
+        predict_resource=True,
+        show_top_features=False,
     ):
         res.status = ResourceStatus.CHECK
 
@@ -300,7 +299,7 @@ class ResourceMgr:
 
     @debug_log_entry_exit(__name__)
     def predict_resource(
-            self, sess: Session, resource: Resource, show_top_features: bool = False
+        self, sess: Session, resource: Resource, show_top_features: bool = False
     ):
         if self.classifier is None:
             raise ValueError("The classifier is not set")
@@ -336,51 +335,62 @@ class ResourceMgr:
 
     @debug_log_entry_exit(__name__)
     def read_resources_from_file(
-        self, sess: Session, absolute_file_path: str, update_resource: bool = False
+        self, sess: Session, file_path: str, update_resource: bool = False
     ):
-        # TODO: poor code, needs refactoring
-        file_path = Path(absolute_file_path)
-        if file_path.is_file():
-            with open(absolute_file_path, encoding="utf-8") as f:
-                lines = [x.strip() for x in f.readlines()]
-                for line in lines:
-                    line = line.split(",")
-                    line = [x.strip() for x in line]
+        def prepare_line(val: str) -> list[str]:
+            val = val.strip().split(",")
+            return [word.strip() for word in val]
 
-                    if len(line) > 1:
-                        res_new_pred = int(line[0])
-                        res_new_value = line[1]
-                        res_new_type = line[2]
+        file_path = Path(file_path)
+        if not file_path.is_file():
+            logger.warn("File %s does not exist", file_path)
+            return
 
-                        # optional parameters
-                        res_new_ispropog = False
-                        res_new_lang = Lang.english
+        with open(file_path, encoding="utf-8") as f:
+            for line in f.readlines():
+                if line.strip() == "":
+                    continue
 
-                        if len(line) > 3:
-                            if line[3].lower() == "true":
-                                res_new_ispropog = True
+                line = prepare_line(line)
+                parts = len(line)
 
-                        if len(line) > 4:
-                            if line[4]:
-                                res_new_lang = line[4]
+                if parts < 2:
+                    raise ValueError(
+                        "Something is wrong, the line has not enough parts"
+                    )
 
-                        if update_resource:
-                            resource = self.get_resource_by_value(sess, res_new_value)
-                            if resource:
-                                resource.type = res_new_type
-                                resource.lang = res_new_lang
-                                resource.is_propog = res_new_ispropog
-                                resource.true_rate = res_new_pred
-                                resource.prediction_rate = res_new_pred
-                                sess.add(resource)
-                        else:
-                            resource = Resource(
-                                value=res_new_value,
-                                type=res_new_type,
-                                status="check",
-                                lang=res_new_lang,
-                                is_propog=res_new_ispropog,
-                                true_rate=res_new_pred,
-                                prediction_rate=res_new_pred,
-                            )
-                            self.add_or_get_exist_resource(sess, resource)
+                res_pred = int(line[0])
+                res_value = line[1]
+                res_type = line[2]
+
+                res_is_propog = False
+                res_lang = Lang.english
+
+                if parts > 3 and line[3].lower() == "true":
+                    res_is_propog = True
+
+                if parts > 4 and line[4]:
+                    res_lang = line[4]
+
+                if update_resource:
+                    if resource := self.get_resource_by_value(sess, res_value):
+                        resource.type = res_type
+                        resource.lang = res_lang
+                        resource.is_propog = res_is_propog
+                        resource.true_rate = res_pred
+                        resource.prediction_rate = res_pred
+                        sess.add(resource)
+                    else:
+                        logger.warn("Such a resource cannot be found: {}", res_value)
+                else:
+                    resource = Resource(
+                        value=res_value,
+                        type=res_type,
+                        status=ResourceStatus.CHECK,
+                        lang=res_lang,
+                        is_propog=res_is_propog,
+                        true_rate=res_pred,
+                        prediction_rate=res_pred,
+                    )
+                    self.add_or_get_exist_resource(sess, resource)
+                sess.flush()
